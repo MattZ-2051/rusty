71937df4f1f516f91b98c19c068b9b25eae0fbd5
Matt Zamora <mzamora2899@gmail.com>
2024-05-01 19:24:47 -0700

    rust book basics reading  and hello world

---
+++ notes/hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/mattzamora/Desktop/Desktop/Code/rust/notes/hello_world/hello_cargo/target/debug/hello_cargo: /Users/mattzamora/Desktop/Desktop/Code/rust/notes/hello_world/hello_cargo/src/main.rs
