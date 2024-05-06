ae35ec3c728b5ca908720697a6560dc490033e2e
Matt Zamora <mzamora2899@gmail.com>
2024-05-05 21:50:40 -0700

    update notes

--- notes/hello_world/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d
+++
@@ -1,5 +0,0 @@
-/Users/mattzamora/Desktop/Desktop/Code/rust/notes/hello_world/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03: src/main.rs
-
-/Users/mattzamora/Desktop/Desktop/Code/rust/notes/hello_world/hello_cargo/target/debug/deps/hello_cargo-b15663378fbcff03.d: src/main.rs
-
-src/main.rs:
--- notes/hello_world/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/mattzamora/Desktop/Desktop/Code/rust/notes/hello_world/hello_cargo/target/debug/hello_cargo: /Users/mattzamora/Desktop/Desktop/Code/rust/notes/hello_world/hello_cargo/src/main.rs
